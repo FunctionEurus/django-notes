7b47b60b144b480bce4c84f56a6962003126ca3c
FunctionEurus <function_eurus@bupt.edu.cn>
2024-04-20 14:12:15 +0800

    Message 15: new URL, view and template to display lists

--- lists/views.py
+++ lists/views.py
@@ -8,11 +8,14 @@ def home_page(request):
         # return HttpResponse(request.POST['item_text'])
         # new_item_text = request.POST['item_text']
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-new-page')
     # else:
     #     new_item_text = ''
     # item = Item()
     # item.text = request.POST.get('item_text', '')
     # item.save()
+    return render(request, 'home.html')
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
--- notes/urls.py
+++ notes/urls.py
@@ -19,4 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.home_page, name='home'),
+    path("lists/the-new-page", views.view_list, name='view_list'),
 ]
